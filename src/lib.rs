65921782cd631224171c7692fccdea61ff1fe08b
Sergey Vasilyev <swasilyev@gmail.com>
2025-11-26 12:19:18 +0300

    trait Poly cleaned up

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,10 @@ pub mod lagrange;
 pub mod poly_mul;
 pub mod product_tree;
 pub mod poly_div;
+mod poly_gcd;
 
+use std::ops::RangeBounds;
+use std::slice::SliceIndex;
 use ark_ff::{FftField, Field, Zero};
 use ark_poly::{DenseUVPolynomial, Evaluations, Polynomial, Radix2EvaluationDomain};
 use ark_poly::univariate::DensePolynomial;
@@ -21,63 +24,71 @@ pub type ME<F> = [PE<F>; 4];
 
 pub trait Poly<F: Field> {
     fn with_coeffs(coeffs: Vec<F>) -> Self;
-    fn constant(c: F) -> Self;
+    fn c_xk(c: F, k: usize) -> Self;
     fn xk(k: usize) -> Self;
+    fn constant(c: F) -> Self;
     fn one() -> Self;
     fn x() -> Self;
-    fn slice(&self, from: usize, to: usize) -> Self;
+    // + SliceIndex<[usize]>
+    fn slice<R: RangeBounds<usize> + SliceIndex<[F], Output = [F]>>(&self, range: R) -> Self;
     fn div_xk(&self, n: usize) -> Self;
     fn mul_xk(&self, n: usize) -> Self;
     fn mod_xk(&self, n: usize) -> Self;
     fn lc(&self) -> F;
     fn ct(&self) -> F;
+    /// `c_k + c_{k+1}.X + ... + c_d.X{d-k}`
+    /// Panics if `k > d`. Otherwise returns `f(X)/X^k`
+    fn l_shift(&self, k: usize) -> Self;
+    /// `c_0.X^k + c_1.X^{k+1} + ... + c_d.X^{d+k} = f(X).X^k`
+    fn r_shift(&self, k: usize) -> Self;
 }
 
 impl<F: FftField> Poly<F> for P<F> {
-    fn with_coeffs(coeffs: Vec<F>) -> Self {
-        Self::from_coefficients_vec(coeffs)
+    /// `c[0] + c[1]X + ... + c[n-1]X^{n-1}`
+    fn with_coeffs(c: Vec<F>) -> Self {
+        Self::from_coefficients_vec(c)
     }
 
-    fn xk(k: usize) -> Self {
+    /// `cX^k`
+    fn c_xk(c: F, k: usize) -> Self {
         let mut coeffs = vec![F::zero(); k + 1];
-        coeffs[k] = F::one();
+        coeffs[k] = c;
         Self::with_coeffs(coeffs)
     }
 
+    /// `X^k`
+    fn xk(k: usize) -> Self {
+        Self::c_xk(F::one(), k)
+    }
+
+    /// `X`
     fn x() -> Self {
         Self::xk(1)
     }
 
+    /// `c`
     fn constant(c: F) -> Self {
-        Self::with_coeffs(vec![c])
+        Self::c_xk(c, 0)
     }
 
+    /// `1`
     fn one() -> Self {
         Self::constant(F::one())
     }
 
-    /// Returns a subarray of coefficients `f_k,...,f_{l-1}` as a degree `l - 1 - k` polynomial.
-    fn slice(&self, from: usize, to: usize) -> Self {
-        Self::with_coeffs(self.coeffs[from..to].to_vec())
+    /// Returns a subarray of coefficients as polynomial.
+    fn slice<R: RangeBounds<usize> + SliceIndex<[F], Output = [F]>>(&self, range: R) -> Self {
+        Self::with_coeffs(self.coeffs[range].to_vec())
     }
 
-    fn div_xk(&self, k: usize) -> Self {
-        if k > self.degree() {
-            Self::zero()
-        } else {
-            self.slice(k, self.coeffs.len())
-        }
+    /// `c_k + c_{k+1}.X + ... + c_d.X{d-k}`
+    /// Panics if `k > d`. Otherwise returns `f(X)/X^k`
+    fn l_shift(&self, k: usize) -> Self {
+        self.slice(k..)
     }
 
-    fn mod_xk(&self, k: usize) -> Self {
-        if k > self.degree() {
-            self.clone()
-        } else {
-            self.slice(0, k)
-        }
-    }
-
-    fn mul_xk(&self, k: usize) -> Self {
+    /// `c_0.X^k + c_1.X^{k+1} + ... + c_d.X^{d+k} = f(X).X^k`
+    fn r_shift(&self, k: usize) -> Self {
         let d = self.degree();
         let mut coeffs = Vec::with_capacity(d + k + 1);
         coeffs.extend(vec![F::zero(); k]);
@@ -85,12 +96,35 @@ impl<F: FftField> Poly<F> for P<F> {
         Self::with_coeffs(coeffs)
     }
 
-    /// The leading coefficient `f_d`.
+    /// `f(X) / X^k`
+    fn div_xk(&self, k: usize) -> Self {
+        if k > self.degree() {
+            Self::zero()
+        } else {
+            self.l_shift(k)
+        }
+    }
+
+    /// `f(X) * X^k`
+    fn mul_xk(&self, k: usize) -> Self {
+        self.r_shift(k)
+    }
+
+    /// `f(X) mod X^k`
+    fn mod_xk(&self, k: usize) -> Self {
+        if k > self.degree() {
+            self.clone()
+        } else {
+            self.slice(..k)
+        }
+    }
+
+    /// The leading coefficient `c_d`.
     fn lc(&self) -> F {
         self.coeffs[self.degree()]
     }
 
-    /// The constant term `f_0 = f(0)`.
+    /// The constant term `c_0 = f(0)`.
     fn ct(&self) -> F {
         self.coeffs[0]
     }
